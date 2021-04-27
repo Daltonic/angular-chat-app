5f50290a07d070928a006d4ddbf863b527341c69
Daltonic <darlingtongospel@gmail.com>
2021-04-27 10:35:09 +0100

    resets the app to default

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/youtube-clone'),
+      dir: require('path').join(__dirname, './coverage/angular-chat-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
