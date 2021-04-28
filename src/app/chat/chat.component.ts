52aed4d141d03eaaa6c4555217447a470cb907a6
Daltonic <darlingtongospel@gmail.com>
2021-04-28 12:59:46 +0100

    experiencing error with the comet chat UI Kit

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { CometChatUI } from '../../cometchat-pro-angular-ui-kit/CometChatWorkspace/projects/angular-chat-ui-kit/src/components/CometChatUI/CometChat-Ui/cometchat-ui.module';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
-
   constructor(private auth: AngularFireAuth, private route: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public logOut(): void {
     this.auth
       .signOut()
       .then(() => this.route.navigate(['login']))
-      .catch((error) => console.log(error.message))
+      .catch((error) => console.log(error.message));
   }
-
 }
